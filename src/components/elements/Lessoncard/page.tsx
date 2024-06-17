ea54ed3eecf6778ac26edf7d04a8aaf628fbe57c
Kitagawa19 <104189576+Kitagawa19@users.noreply.github.com>
2024-06-17 12:14:44 +0900

    #4  update LessonList  taking care of media screen

--- src/components/elements/Lessoncard/page.tsx
+++ src/components/elements/Lessoncard/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client'
+import React,{useState} from 'react';
 import Link from 'next/link';
 
 interface LessonCardProps {
@@ -8,13 +9,21 @@ interface LessonCardProps {
 }
 
 export const LessonCard: React.FC<LessonCardProps> = ({ title, description, link }) => {
+  const [ IsHovered,setIsHovered ] = useState(false);
+
+  const  handleMouseEnter = () => setIsHovered(true);
+  const  handleMouseLeave = () => setIsHovered(false);
+
   return (
-    <div className="col max-w-lg  flex flex-col justify-center items-cener">
-        <Link href={link} className="block p-4 border bg-orange-500 text-white  rounded-lg shadow-lg  hover:bg-orange-600 transition hover:scale-105 group">
+    <div className="col flex justify-center items-cener"
+            onMouseEnter={ handleMouseEnter }
+            onMouseLeave={ handleMouseLeave }
+            >
+        <Link href={link} className="block w-full max-w-2xl p-4 border bg-orange-500 text-white  rounded-lg shadow-lg  hover:bg-orange-600 transition hover:scale-105 group">
           <div className="flex flex-col items-center justify-center">
             <h2 className="text-5xl font-bold">{title}</h2>
           </div>
-          <p className="mt-4 opacity-0  group-hover:opacity-100 transition-opacity  duration-300 group-hover:text-white ">
+          <p className={`mt-4 transition-opacity duration-300 group-hover:text-white ${IsHovered ? "opacity-100":"opacity-100 sm:opacity-0 group-hover:opacity-100"}`}>
             {description}
           </p>
         </Link>
--- src/components/layouts/contents/LessonList/index.tsx
+++ src/components/layouts/contents/LessonList/index.tsx
@@ -29,7 +29,7 @@ export const LessonList:React.FC = () => {
   return(
     <div className="p-4 min-h-screen">
       <h1 className="text-4xl font-bold mb-4">レッスン一覧</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-3">
         {lessons.map((lesson) => (
           <LessonCard
             key={lesson.title}
