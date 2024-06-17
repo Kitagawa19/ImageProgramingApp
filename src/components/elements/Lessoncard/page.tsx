161f774ccd4e2a8ad780b70aa5f3500c8de2d549
Kitagawa19 <104189576+Kitagawa19@users.noreply.github.com>
2024-06-17 11:28:14 +0900

    #3 update card  position

--- src/components/elements/Lessoncard/page.tsx
+++ src/components/elements/Lessoncard/page.tsx
@@ -9,12 +9,12 @@ interface LessonCardProps {
 
 export const LessonCard: React.FC<LessonCardProps> = ({ title, description, link }) => {
   return (
-    <div className="col">
-        <Link href={link} className="block p-4 border bg-orange-500 text-white  rounded-lg shadow-lg  hover:bg-orange-600 transition hover:scale-105">
-          <div className="flex items-center mb-2">
-            <h2 className="text-4xl text-center font-bold">{title}</h2>
+    <div className="col max-w-lg  flex flex-col justify-center items-cener">
+        <Link href={link} className="block p-4 border bg-orange-500 text-white  rounded-lg shadow-lg  hover:bg-orange-600 transition hover:scale-105 group">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-5xl font-bold">{title}</h2>
           </div>
-          <p className="mb-4 opacity-0 text-white hover:opacity-100 transition-opacity  duration-300  rounded-xl">
+          <p className="mt-4 opacity-0  group-hover:opacity-100 transition-opacity  duration-300 group-hover:text-white ">
             {description}
           </p>
         </Link>
