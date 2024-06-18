958e22b7c10f1657c1ffb02977c0da7b9743ea0d
Kitagawa19 <kkitagawa16219@gmail.com>
2024-06-18 15:15:35 +0900

    #7 update Image component

--- src/components/layouts/contents/Top/page.tsx
+++ src/components/layouts/contents/Top/page.tsx
@@ -6,7 +6,7 @@ export const Top:React.FC = () => {
   return(
     <div className="flex flex-col items-center justify-center h-screen space-4">
       <div className="absolute top-14 right-8 rotate-left">
-        <Image src="/beginnermark.png"  width={200} height={200} alt="BegginerMark" />
+        <Image src="/beginnermark.png"  width={200} height={200} alt="BegginerMark" priority={true} />
       </div>
       <PrimaryButton label="レッスン->" link="/LessonList" />
       <PrimaryButton label="つくる！->" link="/"/>
