12f90899c1fec7e697e879aff3f1e5399bc67419
Your Name <you@example.com>
2024-06-24 11:21:42 +0900

    #10 add variable problem

--- src/app/Variables/[step]/page.tsx
+++ src/app/Variables/[step]/page.tsx
@@ -13,16 +13,34 @@ interface StepConfig{
 
 const MaxSteps: { [key: number]: StepConfig }  = {
   1: {
-    title: '変数の学習',
+    title: '変数とは',
     description: '変数とは何かを学びます。',
   },
   2: {
-    title: '変数の使い方',
+    title: '変数を使った問題１',
     description: '変数を使ってみましょう。',
     problem: `カゴに入れた数に３を足した数は？`,
     correctAnswer: 8,
   },
   3: {
+    title: '変数を使った問題２',
+    description: '変数を使ってみましょう。',
+    problem: `カゴに入れた数に2を引いた数は？`,
+    correctAnswer: 3,
+  },
+  4: {
+    title: '変数を使った問題３',
+    description: '変数を使ってみましょう。',
+    problem: `カゴに入れた数に2をかけた数は？`,
+    correctAnswer: 10,
+  },
+  5: {
+    title: '変数を使った問題４',
+    description: '変数を使ってみましょう。',
+    problem: `カゴに入れた数を２で割った数は？`,
+    correctAnswer: 8,
+  },
+  6: {
     title: '変数の活用',
     description: '変数を活用してみましょう。',
   },
