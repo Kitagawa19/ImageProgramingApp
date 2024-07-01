f17ea0ca64379bb3b0943770ea5101044f81e17a
Kitagawa19 <104189576+Kitagawa19@users.noreply.github.com>
2024-07-01 15:30:18 +0900

    add   conditionals's question

---
+++ src/components/elements/ConditionalsImage/index.tsx
@@ -0,0 +1,73 @@
+'use client';
+import React, { useState } from 'react';
+
+export const VariableImage:React.FC<{ defaultItemCount:number}> = ({ defaultItemCount }) => {
+  const [itemsInBasket,setItemsInBasket] = useState(defaultItemCount);
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setItemsInBasket(itemsInBasket + 1);
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  const handleClick = () => {
+    setItemsInBasket(itemsInBasket - 1);
+  };
+
+  return(
+    <div className='mt-4'>
+      <div className='flex justify-around'>
+        <div
+          className="w-48 h-48 border-2 border-dashed flex justify-center items-center"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onClick={handleClick}
+          draggable={false}
+        >
+          春
+        </div>
+        <div
+          className="w-48 h-48 border-2 border-dashed flex justify-center items-center"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onClick={handleClick}
+          draggable={false}
+        >
+          夏
+        </div>
+        <div
+          className="w-48 h-48 border-2 border-dashed flex justify-center items-center"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onClick={handleClick}
+          draggable={false}
+        >
+          秋
+        </div>
+        <div
+          className="w-48 h-48 border-2 border-dashed flex justify-center items-center"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onClick={handleClick}
+          draggable={false}
+        >
+          冬
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            src="/Fruits/apple.png"
+            alt="Apple"
+            draggable
+            className="w-48 h-48 cursor-grab mb-4"
+          />
+        </div>
+      </div>
+      <div className="text-center mt-8">
+        <h2 className="text-xl">カゴに入れたアイテムの数: {itemsInBasket}</h2>
+      </div>
+    </div>
+  )
+}
